c02111350285c9cd6e7e575881fc7391be314c1f
Astar75 <rrogea75@gmail.com>
2021-07-13 23:55:55 +0300

    HB01 data layer for books

--- app/src/main/java/com/example/myapplication/domain/BookDomain.kt
+++ app/src/main/java/com/example/myapplication/domain/BookDomain.kt
@@ -4,15 +4,5 @@ import com.example.myapplication.core.Abstract
 import com.example.myapplication.presentation.BookUi
 
 sealed class BookDomain: Abstract.Object<BookUi, Abstract.Mapper.Empty>() {
-    class Success(): BookDomain() {
-        override fun map(mapper: Abstract.Mapper.Empty): BookUi {
-            TODO()
-        }
-    }
-
-    class Fail(errorType: Int): BookDomain() {
-        override fun map(mapper: Abstract.Mapper.Empty): BookUi {
-            TODO("Not yet implemented")
-        }
-    }
+    // TODO: 13.07.2021 fix
 }
